42cdcb685d2c1d0167c4d37bde63823607a2736a
Connor Worthington <worthington.connor@gmail.com>
2020-09-28 16:43:07 +0100

    Creates Space class

---
+++ lib/space.rb
@@ -0,0 +1,11 @@
+class Space
+
+attr_reader :name, :price, :description
+
+  def initialize(name, price, description) 
+    @name = name
+    @price = price
+    @description = description
+  end 
+
+end 
