5b7f7892e5361c83029c39628f7eb77c7e5767d7
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-28 15:25:24 +0100

    File structure.
    Setup files for MakersBnB project.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# makersbnb
+# MakersBnB
+
+
+run:
+```
+psql -c 'CREATE DATABASE makersbnb;'
+psql -d makersbnb -f db/migrations/database_setup.sql
+```
---
+++ db/migrations/database_setup.sql
@@ -0,0 +1,17 @@
+CREATE EXTENSION pgcrypto;
+CREATE TABLE users(
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+  email VARCHAR(30) NOT NULL UNIQUE,
+  name VARCHAR(30) NOT NULL,
+  password TEXT NOT NULL,
+  );
+
+CREATE TABLE spaces(
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(30)
+  description VARCHAR(240) NOT NULL,
+  time TIMESTAMP WITH TIME ZONE NOT NULL,
+  price MONEY,
+  userid UUID REFERENCES users(id)
+  );
+
