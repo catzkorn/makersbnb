7bcabe4967b1ad4253307b4b44e84c53b5800bce
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-28 16:31:39 +0100

    Created DatabaseConnection methods.
    Created methods in DatabaseConnection class to access databases.

--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -3,15 +3,8 @@ require "./lib/database_connection"
 describe DatabaseConnection do
   describe "#.setup" do
     it "connects to a specific database" do
-      expect(PG).to recieve(:connect).with(dbname: "makersbnb_test")
+      expect(PG).to receive(:connect).with(dbname: "makersbnb_test")
       DatabaseConnection.setup("makersbnb_test")
-    end
-  end
-
-  describe "#.connection" do
-    it "checks the connection to the database" do
-      connection = DatabaseConnection.setup("makersbnb_test")
-      expect(DatabaseConnection.connection).to eq connection
     end
   end
 
@@ -19,7 +12,7 @@ describe DatabaseConnection do
     it "completes a query" do
       connection = DatabaseConnection.setup("makersbnb_test")
 
-      expect(connection).to recieve(:exec).with("SELECT * FROM users;")
+      expect(connection).to receive(:exec).with("SELECT * FROM users;")
       DatabaseConnection.query("SELECT * FROM users;")
     end
   end
