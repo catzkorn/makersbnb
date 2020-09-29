da946c06845545216c72405dc60958e32cc81333
Student <student@Makerss-Air.broadband>
2020-09-29 11:37:27 +0100

    unit test for .all method

--- spec/unit/space_manager_spec.rb
+++ spec/unit/space_manager_spec.rb
@@ -3,6 +3,7 @@ require 'space'
 
 describe SpaceManager do 
   let(:test){Space.new('london flat', '35', 'a beautiful flat in central london lol')}
+  let(:test2){Space.new('manchester flat' , '30', 'a cottage in the outskirt')}
   describe '#.create' do 
     it 'creates new space with Space class passed in as parameter' do 
       space = SpaceManager.create(test)
@@ -16,7 +17,10 @@ describe SpaceManager do
 
   describe '#.all' do
     it 'returns a list of all the spaces' do
-      
+      SpaceManager.create(test)
+      SpaceManager.create(test2)
+      list_spacemanager_all = SpaceManager.all
+      expect(list_spacemanager_all[1].name).to eq 'manchester flat'     
     end
   end
 
