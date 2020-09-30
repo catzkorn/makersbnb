01a8f343e18dd0a702de17e47891a0cb35871aff
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-30 09:49:33 +0100

    Added add_test_user method.
    Added a test user method to be able get a user id for testing purposes.

--- spec/features/test_helpers.rb
+++ spec/features/test_helpers.rb
@@ -10,3 +10,9 @@ def clear_users_table(database = "makersbnb_test")
   connection = PG.connect(dbname: database)
   connection.exec("TRUNCATE TABLE users CASCADE")
 end
+
+def add_test_user(database = "makersbnb_test")
+  connection = PG.connect(dbname: database)
+  result = connection.exec("INSERT INTO users (email, name, password) VALUES ('test@test.com', 'Test McTestason', crypt('password123', gen_salt('bf') ), true) RETURNING id;")
+  return result.values[0][0]
+end
