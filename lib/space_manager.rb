97ca545b0757fe6d5743e6ec077b54faa71838ff
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-30 17:49:00 +0100

    End of day push.

--- lib/space_manager.rb
+++ lib/space_manager.rb
@@ -8,10 +8,12 @@ class SpaceManager
   end
 
   def self.create(space_object)
-    result = DatabaseConnection.query("INSERT INTO spaces (name, description, price, userid, date_availability) VALUES ($1, $2, $3, $4, '{#{space_object.date.join(",")}}') RETURNING id, name, description, price, date_availability, userid;", [space_object.name, space_object.description, space_object.price, space_object.user_id])
+    result = DatabaseConnection.query("INSERT INTO spaces (name, description, price, userid, date_availability) VALUES ($1, $2, $3, $4, '{'$5'}') RETURNING id, name, description, price, date_availability, userid;", [space_object.name, space_object.description, space_object.price, space_object.user_id, space_object.date])
     date_availability = result[0]["date_availability"]
     Space.new(result[0]["name"], result[0]["price"], result[0]["description"], date_availability.gsub(/[{}]/, "").split(","), result[0]["id"], result[0]["userid"])
   end
+
+  # #{space_object.date.join(",")}
 
   def self.user_spaces(user_id)
     result = DatabaseConnection.query("SELECT * FROM spaces WHERE userid = $1;", [user_id])
--- spec/features/list_multiple_spaces_spec.rb
+++ spec/features/list_multiple_spaces_spec.rb
@@ -1,24 +1,25 @@
-feature 'A user can list multiple spaces' do
-  scenario 'user creates a few spaces and checks they are available' do
+feature "A user can list multiple spaces" do
+  scenario "user creates a few spaces and checks they are available" do
+    add_test_user()
     visit("/")
     click_link("Login")
     fill_in(:email, with: "test@test.com")
     fill_in(:password, with: "password123")
     click_button("login")
-    visit('/spaces')
-    click_button('Add Space')
-    fill_in('name', with: 'london flat')
-    fill_in('price', with: '35')
-    fill_in('description', with: 'a beautiful london flat in central london')
-    click_button 'Create space'
+    visit("/spaces")
+    click_button("Add Space")
+    fill_in("name", with: "london flat")
+    fill_in("price", with: "35")
+    fill_in("description", with: "a beautiful london flat in central london")
+    click_button "Create space"
     sleep(1)
-    click_button('Add Space')
-    fill_in('name', with: 'manchester flat')
-    fill_in('price', with: '20')
-    fill_in('description', with: 'a beautiful manchester flat in central manchester')
-    click_button 'Create space'
+    click_button("Add Space")
+    fill_in("name", with: "manchester flat")
+    fill_in("price", with: "20")
+    fill_in("description", with: "a beautiful manchester flat in central manchester")
+    click_button "Create space"
     sleep(1)
-    expect(page).to have_content 'london flat'
-    expect(page).to have_content 'manchester flat'
+    expect(page).to have_content "london flat"
+    expect(page).to have_content "manchester flat"
   end
 end
