74081a55e201416a1ec038d49ea7d6706f447d8a
Student <student@Makerss-Air.broadband>
2020-10-02 12:00:46 +0100

    fixed the failing test

--- spec/features/create_spec.rb
+++ spec/features/create_spec.rb
@@ -1,11 +1,12 @@
 feature "Creates a new space" do #first feature test pretending login does not exist
   scenario "User creates a new space with name, price and description" do
+    user = add_test_user()
     visit("/")
     click_on("Login")
     fill_in("email", with: "test@test.com")
-    fill_in("password", with: "password")
+    fill_in("password", with: "password123")
     click_on("login")
-    visit("/spaces/new")
+    click_link("Add Space")
     fill_in("name", with: "london flat")
     fill_in("price", with: "35")
     fill_in("description", with: "a beautiful london flat in central london lol")
