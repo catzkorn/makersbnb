b6acb589d907502ddf19df2fd18f85dffdaa9bd9
Student <student@Makerss-Air.broadband>
2020-10-02 14:50:47 +0100

    testing the database connection
    .setup, .query and takes two arguements

--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -3,13 +3,15 @@ require "./lib/database_connection"
 describe DatabaseConnection do
   describe "#.setup" do
     it "connects to a specific database" do
-      expect(PG).to receive(:connect).with(dbname: "makersbnb_test")
-      DatabaseConnection.setup("makersbnb_test")
+     expect(DatabaseConnection.setup("makersbnb_test")).to be_kind_of(PG::Connection)
     end
   end
 
   describe "#.query" do
     it "completes a query" do
+      expect(DatabaseConnection.query("SELECT * FROM users;")).to be_kind_of(PG::Result)
+    end
+    it "takes two arguements" do 
       connection = DatabaseConnection.setup("makersbnb_test")
       expect(connection).to receive(:exec).with("SELECT * FROM users;", [])
       DatabaseConnection.query("SELECT * FROM users;")
